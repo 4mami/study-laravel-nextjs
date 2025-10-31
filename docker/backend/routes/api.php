6084d8aca88b8dc4eaa8829d952bc85d66292ce5
4mami <76460459+4mami@users.noreply.github.com>
2025-10-31 22:19:56 +0900

    [mod] SPA認証用の設定

--- docker/backend/routes/api.php
+++ docker/backend/routes/api.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\NoteController;
 // ログインと新規登録のルーティング
 Route::post('/register', 'App\Http\Controllers\AuthController@register');
 Route::post('/login', 'App\Http\Controllers\AuthController@login');
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
