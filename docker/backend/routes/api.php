117204109e3d6a52c22dbcf0afe5fdc525027456
4mami <76460459+4mami@users.noreply.github.com>
2025-11-01 17:11:13 +0900

    [add] 生徒の操作用APIを実装

--- docker/backend/routes/api.php
+++ docker/backend/routes/api.php
@@ -9,3 +9,12 @@ Route::post('/register', 'App\Http\Controllers\AuthController@register');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->group(function() {
+    // studentsのCRUDのルーティング
+    Route::get('/students', 'App\Http\Controllers\StudentController@index');
+    Route::post('/students', 'App\Http\Controllers\StudentController@store');
+    Route::get('/students/{student:id}', 'App\Http\Controllers\StudentController@edit');
+    Route::patch('/students/{student:id}', 'App\Http\Controllers\StudentController@update');
+    Route::delete('/students/{student:id}', 'App\Http\Controllers\StudentController@delete');
+});
