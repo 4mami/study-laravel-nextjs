802066483b196a2d27ec13f439a04b2ad373e8ad
4mami <76460459+4mami@users.noreply.github.com>
2025-11-01 17:08:14 +0900

    [fix] /login はapiではなくwebルートに作成する必要があったので修正

--- docker/backend/routes/api.php
+++ docker/backend/routes/api.php
@@ -5,7 +5,6 @@ use App\Http\Controllers\NoteController;
 
 // ログインと新規登録のルーティング
 Route::post('/register', 'App\Http\Controllers\AuthController@register');
-Route::post('/login', 'App\Http\Controllers\AuthController@login');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
